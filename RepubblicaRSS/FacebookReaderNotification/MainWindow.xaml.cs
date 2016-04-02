197243c8dc2808d889b625f4f903ce42b991c5ca
Mirko Bastianini <mirko.bastianini@gmail.com>
2016-04-02 12:11:40 +0200

    Added files via upload

---
+++ RepubblicaRSS/FacebookReaderNotification/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Web;
+using System.Xml;
+
+namespace FacebookReaderNotification
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ParseRssFile();
+           
+       
+            
+        }
+        private string ParseRssFile()
+        {
+            XmlDocument rssXmlDoc = new XmlDocument();
+
+            // Load the RSS file from the RSS URL
+            rssXmlDoc.Load("http://www.repubblica.it/rss/homepage/rss2.0.xml");
+
+            // Parse the Items in the RSS file
+            XmlNodeList rssNodes = rssXmlDoc.SelectNodes("rss/channel/item");
+
+            StringBuilder rssContent = new StringBuilder();
+
+            // Iterate through the items in the RSS file
+            foreach (XmlNode rssNode in rssNodes)
+            {
+                
+                XmlNode rssSubNode = rssNode.SelectSingleNode("title");
+                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("author");
+                string author = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("pubDate");
+                string pubDate = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssContent.Append(title + "\n" + author+ "\n" + pubDate+"\n\n");
+                listView.Items.Add(rssContent.ToString());
+                rssContent.Clear();
+            }
+
+            // Return the string that contain the RSS items
+            return "";
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listView.Items.Clear();
+            ParseRssFile();
+        }
+    }
+}
